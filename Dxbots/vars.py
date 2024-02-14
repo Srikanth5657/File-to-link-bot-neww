b694a98f73c93ccb53f0aea36c28e243ba476bcb
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2024-02-14 19:10:49 +0530

    Update vars.py

--- Dxbots/vars.py
+++ Dxbots/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', 26181458))
+    API_HASH = str(getenv('API_HASH', "09c734fa0c97721650b5dc0cdbd679d7"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "6670480122:AAGuyxOSpvonK6eq3CfkKeNMsUdF1KEwZgc"))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', -1001943293981))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", 6077206903).split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "Banoth_Srikanth"))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
